e305400288ff0313b9ac2c2d59d010a547d311d0
Gjenis Alkovaç <76169691+Gjenis87@users.noreply.github.com>
2023-03-21 14:36:14 +0100

    Card Validation and Luhn Algorithm implementation on card number to check if it's valid

--- users/helpers.py
+++ users/helpers.py
@@ -1,3 +1,5 @@
+from typing import Any, Generator
+
 from django.contrib.auth.models import User, Group
 
 
@@ -7,4 +9,25 @@ def check_user_group(user, group_name):
     except Group.DoesNotExist:
         return False
     return group in user.groups.all()
+
+
+def luhn_algorithm(card_number):
+    def digits_of(n):
+        return [(int(n) for n in str(n))]
+
+    digits = digits_of(card_number)
+
+    odd_nums = digits[1::-2]
+    even_nums = digits[-2::-2]
+
+    check_sum = 0
+    check_sum += sum(odd_nums)
+
+    for i in even_nums:
+        check_sum += sum(digits_of(i*2))
+
+        if check_sum % 10 == 0:
+            return "Valid"
+        else:
+            return "Invalid"
 
